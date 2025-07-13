7bed402bdd2da2cfc90be0e3b30d99fa2542c278
Haokun Chen <chenhaokun24549@gmail.com>
2025-07-13 15:14:26 +0200

    Update train.sh

---
+++ train.sh
@@ -0,0 +1,24 @@
+DATASET='domainnet'
+if [[ "$DATASET" == "officehome" ]]; then
+    domains=("Art" "Clipart" "Product" "Real")
+elif [[ $DATASET == "domainnet" ]]; then
+    domains=("clipart" "infograph" "painting" "quickdraw" "real" "sketch")
+elif [[ $DATASET == "pacs" ]]; then
+    domains=("photo" "sketch" "art_painting" "cartoon")
+else
+    domains=("client_0"  "client_1" "client_2" "client_3" "client_4")
+fi
+
+idx=0
+TRAIN_TYPE=fedlip_prompt
+for domain in "${domains[@]}"
+do
+    echo "Training domain: $domain"
+    CUDA_VISIBLE_DEVICES=$idx python train.py \
+        --output_dir "exps_$DATASET/$TRAIN_TYPE"_d_"$domain"_multiclient --evaluation_type generate \
+        --num_train_epochs 50 --train_batch_size 8 \
+        --domain "$domain" --train_type $TRAIN_TYPE \
+        --dataset "$DATASET" --num_shot 16 \
+        --learning_rate 0.1 &
+    idx=$((idx+1))
+done
